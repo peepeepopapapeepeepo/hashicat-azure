237a387514da37f7db259a2c67029c0d27b3c9da
Sawit Meekwamdee <peepeepopapapeepeepo@gmail.com>
2022-04-22 23:09:45 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/peepeepopapapeepeepo/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "sawit-hashicat-azure"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
